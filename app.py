482f6d38632d27b762665fa24127555a2647a251
Luis Goncalves <luisgoncalvesfigueira@gmail.com>
2024-10-07 18:56:32 +0000

    Fix issues with instaled packages and reatrain the model architecture to add more filter in the third convolutional Layer and make sure that the shape will be 100x100.

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+import sys
+import os
+
+# Assuming 'app_pages' is in the current working directory
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app_pages'))
+
+from app_pages.page_mildew_detector import page_mildew_detector_body
 
 # load pages scripts
 from app_pages.page_project_summary import page_project_sumary_body
--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -3,47 +3,46 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
-from src.data_management import download_dataframe_as_csv
-from src.machine_learning.predictive_analysis import (
-                                                    load_model_and_predict,
-                                                    resize_input_image,
-                                                    plot_predictions_probabilities
-                                                    )
+from .src.data_management import download_dataframe_as_csv  # Correct relative import
+from .src.machine_learning.predictive_analysis import (
+    load_model_and_predict,
+    resize_input_image,
+    plot_predictions_probabilities
+)
 
 def page_mildew_detector_body():
     st.info(
-        f"The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew."
+        "The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew."
     )
 
     st.write(
-        f"Here you can download a set of mildew or healthy leaf for live prediction. "
-        f"You can download the images from [here](https://www.kaggle.com/datasets/codeinstitute/cherry-leaves"
+        "Here you can download a set of mildew or healthy leaves for live prediction. "
+        "You can download the images from [here](https://www.kaggle.com/datasets/codeinstitute/cherry-leaves)"
     )
     st.write("---")
 
-    images_buffer = st.file_uploader('Upload leaf image. You may select more than one.',
-                                        type='png',accept_multiple_files=True)
+    images_buffer = st.file_uploader(
+        'Upload leaf image. You may select more than one.',
+        type='png', 
+        accept_multiple_files=True
+    )
    
-    if images_buffer is not None:
-        df_report = pd.DataFrame([])
+    if images_buffer:
+        df_report = pd.DataFrame([])  # Initialize the DataFrame outside the loop
         for image in images_buffer:
-
-            img_pil = (Image.open(image))
+            img_pil = Image.open(image)
             st.info(f"Leaf Image: **{image.name}**")
             img_array = np.array(img_pil)
             st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
 
-            version = 'v1'
+            version = 'v1'  # Make sure to define version here
             resized_img = resize_input_image(img=img_pil, version=version)
             pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
             plot_predictions_probabilities(pred_proba, pred_class)
 
-            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
-                                        ignore_index=True)
+            df_report = df_report.append({"Name": image.name, 'Result': pred_class}, ignore_index=True)
         
         if not df_report.empty:
             st.success("Analysis Report")
             st.table(df_report)
             st.markdown(download_dataframe_as_csv(df_report), unsafe_allow_html=True)
-
-

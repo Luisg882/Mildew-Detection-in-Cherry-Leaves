fa07581d48f0212413b62d845fe4daecbe776096
Luis Goncalves <luisgoncalvesfigueira@gmail.com>
2024-10-07 16:23:49 +0000

    Create page_mildew_detector.py to manage te mildew image predictions. Add src folder with the machine learning functions to make manage the logic of the mildew detector

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_project_summary import page_project_sumary_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_mildew_detector import page_mildew_detector_body
+
 
 
 app = MultiPage(app_name="Mildew Detector")  # Create an instance of the app
@@ -12,5 +14,6 @@ app = MultiPage(app_name="Mildew Detector")  # Create an instance of the app
 
 app.add_page("Project Summary", page_project_sumary_body)
 app.add_page("Leaves Visualizer", page_leaves_visualizer_body)
+app.add_page("Mildew Detector", page_mildew_detector_body)
 
 app.run()
